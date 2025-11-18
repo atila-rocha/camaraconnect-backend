10091397821d9dac93805ac5fb1760bd0e1e1dde
Átila Carvalho Rocha <atilacarvalhorocha@hotmail.com>
2025-11-18 18:54:33 -0300

    feat: Spring initializer

---
+++ camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/CamaraconnectApplication.kt
@@ -0,0 +1,11 @@
+package com.unifor.br.camaraconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamaraconnectApplication
+
+fun main(args: Array<String>) {
+	runApplication<CamaraconnectApplication>(*args)
+}
---
+++ camaraconnect/src/test/kotlin/com/unifor/br/camaraconnect/CamaraconnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unifor.br.camaraconnect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamaraconnectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
