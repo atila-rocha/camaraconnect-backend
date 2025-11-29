d7db9c9ae44ff474e36170d5b1c14a7b147f2bea
Átila Carvalho Rocha <atilacarvalhorocha@hotmail.com>
2025-11-29 19:54:38 -0300

    feat: user factory

---
+++ camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/factory/UserFactory.kt
@@ -0,0 +1,30 @@
+package com.unifor.br.camaraconnect.factory
+
+import com.unifor.br.camaraconnect.api.controller.dto.request.UserRequestDTO
+import com.unifor.br.camaraconnect.api.controller.dto.response.UserResponseDTO
+import com.unifor.br.camaraconnect.repository.model.User
+import org.springframework.stereotype.Component
+
+
+@Component
+class UserFactory {
+    fun userResquestToUser(dto: UserRequestDTO): User{
+        return User(
+            username = dto.username,
+            password = dto.password,
+            email = dto.email,
+            role = dto.role,
+        )
+    }
+
+    fun userToUserResponse(user: User): UserResponseDTO{
+        return UserResponseDTO(
+            userId = user.userId!!,
+            username = user.username,
+            password = user.password,
+            email = user.email,
+            role = user.role,
+
+        )
+    }
+}
