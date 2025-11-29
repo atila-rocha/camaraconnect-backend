fd1618f2a5c1390ffd9576f476ff31d9b1d055b3
Átila Carvalho Rocha <atilacarvalhorocha@hotmail.com>
2025-11-29 19:55:07 -0300

    feat: User controller and Service

--- camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/api/controller/UserController.kt
+++ camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/api/controller/UserController.kt
@@ -1,4 +1,69 @@
 package com.unifor.br.camaraconnect.api.controller
 
-class UserController {
+import com.unifor.br.camaraconnect.api.controller.dto.request.UserRequestDTO
+import com.unifor.br.camaraconnect.api.controller.dto.response.UserResponseDTO
+import com.unifor.br.camaraconnect.api.service.UserService
+import com.unifor.br.camaraconnect.factory.UserFactory
+import com.unifor.br.camaraconnect.repository.model.User
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    private val userService: UserService,
+    private val userFactory: UserFactory,
+) {
+    @PostMapping
+    fun createUser(@RequestBody userRequestDTO: UserRequestDTO): ResponseEntity<UserResponseDTO>{
+        val userOptional = userService.createUser(userFactory.userResquestToUser(userRequestDTO))
+        if (userOptional.isEmpty){
+            return ResponseEntity.badRequest().build()
+        }
+        return ResponseEntity(userFactory.userToUserResponse(userOptional.get()), HttpStatus.OK)
+    }
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable id: Int, @RequestBody userRequestDTO: UserRequestDTO): ResponseEntity<UserResponseDTO>{
+        val userOptional = userService.updateUser(id, userFactory.userResquestToUser(userRequestDTO))
+        if (userOptional.isEmpty){
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity(userFactory.userToUserResponse(userOptional.get()), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUser(@PathVariable id: Int): ResponseEntity<UserResponseDTO>{
+        val userOptional = userService.deleteUser(id)
+        if (userOptional.isEmpty){
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity(userFactory.userToUserResponse(userOptional.get()), HttpStatus.NO_CONTENT)
+    }
+
+    @GetMapping("/{id}")
+    fun getuserById(@PathVariable id: Int): ResponseEntity<UserResponseDTO>{
+        val userOptional = userService.findUserById(id)
+        if (userOptional.isEmpty){
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity.ok(userFactory.userToUserResponse(userOptional.get()))
+    }
+
+    @GetMapping
+    fun getAllUsers(): ResponseEntity<List<UserResponseDTO>>{
+        val users = userService.findAllUsers()
+        val userResponseDTOList = users.map { userFactory.userToUserResponse(it) }
+        return ResponseEntity.ok(userResponseDTOList)
+
+    }
+
 }
--- camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/api/service/UserService.kt
+++ camaraconnect/src/main/kotlin/com/unifor/br/camaraconnect/api/service/UserService.kt
@@ -1,4 +1,52 @@
 package com.unifor.br.camaraconnect.api.service
 
-class UserService {
+import com.unifor.br.camaraconnect.repository.UserRepository
+import com.unifor.br.camaraconnect.repository.model.User
+import org.springframework.stereotype.Service
+import java.util.Optional
+
+
+@Service
+class UserService (
+    private val  userRepository: UserRepository
+){
+    fun createUser(user: User): Optional<User>{
+        val newUser = userRepository.save(user)
+        return Optional.of(newUser)
+    }
+    fun updateUser(id:Int, user: User): Optional<User>{
+        val userOptional = userRepository.findById(id)
+        if (userOptional.isEmpty){
+            return Optional.empty()
+        }
+        val userToUpdate= User(
+            userId = userOptional.get().userId,
+            username=user.username,
+            password=user.password,
+            email=user.email
+        )
+        val updatedUser= userRepository.save(userToUpdate)
+        return Optional.of(updatedUser)
+    }
+
+    fun deleteUser(id:Int): Optional<User>{
+        val userOptional = userRepository.findById(id)
+        if (userOptional.isEmpty){
+            return Optional.empty()
+        }
+        userRepository.deleteById(id)
+        return Optional.of(userOptional.get())
+    }
+
+    fun findUserById(id: Int): Optional<User>{
+        val userOptional = userRepository.findById(id)
+        if (userOptional.isEmpty){
+            return Optional.empty()
+        }
+        return Optional.of(userOptional.get())
+    }
+
+    fun findAllUsers(): List<User>{
+        return userRepository.findAll()
+    }
 }
